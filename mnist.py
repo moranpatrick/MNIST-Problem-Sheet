af1e37d35b4550fdf8210cd9b1728f72465a1bc7
moranpatrick <patrickmoran121@gmail.com>
2017-10-04 20:51:54 +0100

    Updated Readme

--- mnist.py
+++ mnist.py
@@ -9,5 +9,12 @@ import gzip
 #open the file
 f = gzip.open('data/train-images-idx3-ubyte.gz', 'rb')
 
+# Get the magic number which we know is 2051
+magic = f.read(4)
+print(magic)
+
+# Convert the Magic number To Decimal
+print(int.from_bytes(magic, byteorder='big'))
+
 #close the file
 f.close()
